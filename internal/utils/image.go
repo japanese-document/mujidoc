805ec6fe6089dbb4ebf1a27af9bb9e4aadb54df7
38elements <mh19820223@gmail.com>
2024-02-28 22:32:04 +0900

    ImageSize

--- internal/utils/image.go
+++ internal/utils/image.go
@@ -12,6 +12,16 @@ import (
 	_ "golang.org/x/image/webp"
 )
 
+// ImageSize takes the path to an image file and returns the dimensions of the image.
+// Supported image formats include JPEG, PNG, GIF, BMP, TIFF, and WEBP, thanks to
+// the standard image package and additional formats provided by the golang.org/x/image package.
+//
+// Parameters:
+// - path: A string representing the filesystem path to the image file.
+//
+// Returns:
+// - The width (Dx) and height (Dy) of the image as integers.
+// - An error if the file cannot be opened, or if the image format is not recognized or is corrupt.
 func ImageSize(path string) (int, int, error) {
 	file, err := os.Open(path)
 	if err != nil {
